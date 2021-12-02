3956f67115fafa00a0e0a942068fd66f103588c7
daniel-walters <waltersdaniel96@gmail.com>
2021-12-02 16:05:03 +0800

    finishes and tests checkIfUserIsAMember

--- src/Users/userServices.js
+++ src/Users/userServices.js
@@ -1,11 +1,16 @@
 import members from '../../external_db/members.json';
 
+//Returns member object with {membership_number, isStaff} if member exists in external db. 
+//Otherwise returns null
 export function checkIfUserIsAMember(membership_number) {
     const { members: externalMembers } = members;
     const member = externalMembers.find((member) => {
-        const { membership_number: extMemNum, isStaff } = member;
+        const { membership_number: extMemNum } = member;
         return (extMemNum === membership_number);
     });
 
-    return !!member;
+    return {
+        isMember: !!member,
+        isStaff: member ? member.isStaff : null
+    }
 }
--- tests/Users/userServices.test.js
+++ tests/Users/userServices.test.js
@@ -1,10 +1,23 @@
 import { checkIfUserIsAMember } from "../../src/Users/userServices.js";
 
 describe('checkIfUserIsAMember', () => {
-    test('Returns true if membership number is found in the external database', () => {
-        expect(checkIfUserIsAMember(1111)).toEqual(true);
+    //Returns an Object
+    test('Returns an object', () => {
+        expect(typeof(checkIfUserIsAMember(1111))).toBe('object');
     });
-    test('Returns false if membership number is not found in the external database', () => {
-        expect(checkIfUserIsAMember(0)).toEqual(false);
+    
+    test('Existing staff member returns object with isMember: true, isStaff: true', () => {
+        expect(checkIfUserIsAMember(1111).isMember).toEqual(true);
+        expect(checkIfUserIsAMember(1111).isStaff).toEqual(true);
+    });
+
+    test('Existing regular member returns object with isMember: true, isStaff: false', () => {
+        expect(checkIfUserIsAMember(3333).isMember).toEqual(true);
+        expect(checkIfUserIsAMember(3333).isStaff).toEqual(false);
+    })
+    
+    test('Non-Existing member returns object with isMember: false, isStaff: null', () => {
+        expect(checkIfUserIsAMember(0).isMember).toEqual(false);
+        expect(checkIfUserIsAMember(0).isStaff).toBeNull();
     });
 });
