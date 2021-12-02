cc5aa34a0730af35638ae59140e530e147742c60
daniel-walters <waltersdaniel96@gmail.com>
2021-12-02 16:47:39 +0800

    refactors userServices tests

--- tests/Users/userServices.test.js
+++ tests/Users/userServices.test.js
@@ -1,23 +1,31 @@
 import { checkIfUserIsAMember } from "../../src/Users/userServices.js";
 
 describe('checkIfUserIsAMember', () => {
-    //Returns an Object
     test('Returns an object', () => {
         expect(typeof(checkIfUserIsAMember(1111))).toBe('object');
     });
-    
-    test('Existing staff member returns object with isMember: true, isStaff: true', () => {
-        expect(checkIfUserIsAMember(1111).isMember).toEqual(true);
-        expect(checkIfUserIsAMember(1111).isStaff).toEqual(true);
-    });
 
-    test('Existing regular member returns object with isMember: true, isStaff: false', () => {
-        expect(checkIfUserIsAMember(3333).isMember).toEqual(true);
-        expect(checkIfUserIsAMember(3333).isStaff).toEqual(false);
-    })
+    describe('existing staff member', () => {
+        let memberInfo = checkIfUserIsAMember(1111);
+        test('Existing staff member returns object with isMember: true, isStaff: true', () => {
+            expect(memberInfo.isMember).toEqual(true);
+            expect(memberInfo.isStaff).toEqual(true);
+        });
+    });
     
-    test('Non-Existing member returns object with isMember: false, isStaff: null', () => {
-        expect(checkIfUserIsAMember(0).isMember).toEqual(false);
-        expect(checkIfUserIsAMember(0).isStaff).toBeNull();
+    describe('existing regular member', () => {
+        let memberInfo = checkIfUserIsAMember(3333);
+        test('Existing regular member returns object with isMember: true, isStaff: false', () => {
+            expect(memberInfo.isMember).toEqual(true);
+            expect(memberInfo.isStaff).toEqual(false);
+        });
+    });
+    
+    describe('non-existing member', () => {
+        let memberInfo = checkIfUserIsAMember(0);
+        test('Non-Existing member returns object with isMember: false, isStaff: null', () => {
+            expect(memberInfo.isMember).toEqual(false);
+            expect(memberInfo.isStaff).toBeNull();
+        });
     });
 });
