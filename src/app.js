14463dea379c4b6ad8faa0d546090f1392d9d0f6
June Du <junedu416@gmail.com>
2021-12-20 23:17:39 +1100

    change report schema to set default value for resolved, fix report type to match front-end

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import cors from 'cors';
 const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://gymappdevelopment.netlify.app'];
 const corsOptions = {
     origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1) {
+      if ( allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true)
       } else {
         callback(new Error('Not allowed by CORS'))
