4f49839f5b7e397b01fc546ae93015bdfc27741f
daniel-walters <waltersdaniel96@gmail.com>
2021-12-28 12:42:50 +1100

    adds 20 exercises to a list of exercises

---
+++ external_db/exercises.js
@@ -0,0 +1,136 @@
+const exercises = [
+    {
+        name: "Bench Press",
+        description: "an upper-body weight training exercise in which the trainee presses a weight upwards while lying on a weight training bench.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Incline Bench Press",
+        description: "A variation on the Bench Press where an incline elevates the shoulders and lowers the pelvis as if reclining in a chair",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Deadlift",
+        description: "a weight training exercise in which a loaded barbell or bar is lifted off the ground to the level of the hips, torso perpendicular to the floor, before being placed back on the ground.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Squat",
+        description: "a strength exercise in which the trainee lowers their hips from a standing position and then stands back up.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Overhead Press",
+        description: "is set up by taking a barbell or other weight and putting it in a racking position. The weight is then pressed to overhead.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Leg Press",
+        description: "a compound weight training exercise in which the individual pushes a weight or resistance away from them using their legs.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Barbell Hip Thrust",
+        description: "an exercise to strengthen the lower back, glute muscles, lower abdominal muscles, and maintain hip muscle balance.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 20,
+    },
+    {
+        name: "Cable Flye",
+        description: "a strength training exercise in which the hand and arm move through an arc while the elbow is kept at a constant angle.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 10,
+    },
+    {
+        name: "Dumbbell Skull Crushers",
+        description: "one of the most stimulating exercises to the entire triceps muscle group in the upper arm.",
+        defaultSets: 3,
+        defaultReps: 10,
+        defaultWeight: 4,
+    },
+    {
+        name: "Leg Curl",
+        description: "an isolation exercise that targets the hamstring muscles.",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 10,
+    },
+    {
+        name: "Leg Extension",
+        description: "a resistance weight training exercise that targets the quadriceps muscle in the legs",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 10,
+    },
+    {
+        name: "Calf Raise",
+        description: "a method of exercising the gastrocnemius, tibialis posterior, peroneals and soleus muscles of the lower leg.",
+        defaultSets: 2,
+        defaultReps: 12,
+        defaultWeight: 5,
+    },
+    {
+        name: "Lat Pulldown",
+        description: "a strength training exercise designed to develop the latissimus dorsi muscle",
+        defaultSets: 3,
+        defaultReps: 6,
+        defaultWeight: 10,
+    },
+    {
+        name: "Bicep Curl",
+        description: "an exercise that primarily targets the biceps brachii muscle. It may be performed using a barbell, dumbbell, resistance band, or other equipment.",
+        defaultSets: 3,
+        defaultReps: 12,
+        defaultWeight: 5,
+    },
+    {
+        name: "Dumbell Lateral Raise",
+        description: "works the deltoid muscle of the shoulder. The movement starts with the arms straight, and the hands holding weights at the sides or in front of the body.",
+        defaultSets: 3,
+        defaultReps: 12,
+        defaultWeight: 3,
+    },
+    {
+        name: "Cable Seated Row",
+        description: "an exercise where the purpose is to strengthen the muscles that draw the rower's arms toward the body as well as those that retract the scapulae and those that support the spine.",
+        defaultSets: 3,
+        defaultReps: 8,
+        defaultWeight: 10,
+    },
+    {
+        name: "Indoor Rower",
+        description: "a machine used to simulate the action of watercraft rowing for the purpose of exercise or training for rowing",
+        defaultDistance: 1000
+    },
+    {
+        name: "Elliptical",
+        description: "a stationary exercise machine used to stair climb, walk, or run without causing excessive pressure to the joints, hence decreasing the risk of impact injuries.",
+        defaultDistance: 1000
+    },
+    {
+        name: "Stationary Bicycle",
+        description: "a device used as exercise equipment for indoor cycling.",
+        defaultDistance: 1000
+    },
+    {
+        name: "Treadmill",
+        description: "a device generally used for walking, running, or climbing while staying in the same place. ",
+        defaultDistance: 1000
+    },
+]
+
+export default exercises;
