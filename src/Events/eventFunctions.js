31c883854e78722589dcc16f10ee5e95fbeb3ea6
ahnemui <clh.oneil@gmail.com>
2021-12-31 13:28:58 +1100

    adds createdBy column to events schema

--- src/Events/eventFunctions.js
+++ src/Events/eventFunctions.js
@@ -22,6 +22,7 @@ export async function createEvent(details){
         endTime:details.endTime,
         registeredUsers:details.registeredUsers,
         spotsAvailable: details.spotsAvailable,
+        createdBy: details.createdBy,
         eventImage: details.eventImage,
         category: details.category    
     })
@@ -39,6 +40,7 @@ export async function updateEventById(id, details){
         endTime:details.endTime,
         registeredUsers:details.registeredUsers,
         spotsAvailable: details.spotsAvailable,
+        createdBy: details.createdBy,
         eventImage: details.eventImage,
         category: details.category    
     }
