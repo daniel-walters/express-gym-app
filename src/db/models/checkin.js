a17d67781f2a63790e122e19d0a7d22bb7113548
daniel-walters <waltersdaniel96@gmail.com>
2022-01-06 10:28:41 +1100

    updates checkin model to store stats

--- src/db/models/checkin.js
+++ src/db/models/checkin.js
@@ -2,6 +2,17 @@ import mongoose from "../index.js";
 
 const checkInSchema = new mongoose.Schema({
     numCheckedIn: Number,
+    dailyStats: {
+        Sunday: Number,
+        Monday: Number,
+        Tuesday: Number,
+        Wednesday: Number,
+        Thursday: Number,
+        Friday: Number,
+        Saturday: Number
+    }
+}, {
+    timestamps: true
 });
 
 export default mongoose.model("CheckIn", checkInSchema);
