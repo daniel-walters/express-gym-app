140bbf2077a41145fab11e1106b6d39d1073388c
daniel-walters <waltersdaniel96@gmail.com>
2022-01-06 10:53:49 +1100

    increments day at which user checks in. keeps track of how many weeks since the doc has been created

--- src/db/models/checkin.js
+++ src/db/models/checkin.js
@@ -3,14 +3,15 @@ import mongoose from "../index.js";
 const checkInSchema = new mongoose.Schema({
     numCheckedIn: Number,
     dailyStats: {
-        Sunday: Number,
-        Monday: Number,
-        Tuesday: Number,
-        Wednesday: Number,
-        Thursday: Number,
-        Friday: Number,
-        Saturday: Number
-    }
+        Sunday: {type: Number, default: 0},
+        Monday: {type: Number, default: 0},
+        Tuesday: {type: Number, default: 0},
+        Wednesday: {type: Number, default: 0},
+        Thursday: {type: Number, default: 0},
+        Friday: {type: Number, default: 0},
+        Saturday: {type: Number, default: 0},
+    },
+    weeksActive: {type: Number, default: 1}
 }, {
     timestamps: true
 });
