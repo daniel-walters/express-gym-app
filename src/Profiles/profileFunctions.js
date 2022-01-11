02c9f4a256ab9d5f198e827321a0d7979d60bd68
Nam <namnguyen13_@hotmail.com>
2022-01-11 18:59:44 +1100

    Adds email to profile and profile image.

--- src/Profiles/profileFunctions.js
+++ src/Profiles/profileFunctions.js
@@ -1,32 +1,40 @@
-import externalMembers from '../../external_db/members.js';
-import Profile from '../db/models/profileSchema.js';
+import externalMembers from "../../external_db/members.js";
+import Profile from "../db/models/profileSchema.js";
 
 export const checkIfUserIsAStaff = (number) => {
-    const member = externalMembers.find(existingMember => existingMember.membershipNumber === number);
-    return member.isStaff
+  const member = externalMembers.find(
+    (existingMember) => existingMember.membershipNumber === number
+  );
+  return member.isStaff;
+};
+
+export async function getProfileByUid(uid) {
+  let profile = await Profile.findOne({ userId: uid }).populate({
+    path: "workouts.exercises.exerciseId",
+    select: ["name"],
+  });
+  return profile;
 }
 
+export async function updateProfileByUid(uid, details) {
+  const updatedProfileDetails = {
+    userId: details.userId,
+    firstName: details.firstName,
+    lastName: details.lastName,
+    description: details.description,
+    isStaff: details.isStaff,
+    weight: details.weight,
+    checkedIn: details.checkedIn,
+    workouts: details.workouts,
+    email: details.email,
+    photo: details.photo,
+  };
 
-export async function getProfileByUid(uid){
-    let profile = await Profile.findOne({userId: uid}).populate({path:'workouts.exercises.exerciseId', select: ['name']})
-    return profile
-}
-
-export async function updateProfileByUid(uid, details){
-    const updatedProfileDetails = {
-            userId: details.userId,
-            firstName: details.firstName,
-            lastName: details.lastName,
-            description: details.description,
-            isStaff: details.isStaff,
-            weight: details.weight,
-            checkedIn: details.checkedIn,
-            workouts: details.workouts,
-    }
-    
-    let updatedProfile = await Profile.findOneAndUpdate({userId: uid}, updatedProfileDetails, { returnOriginal: false })
-        .populate({path:'workouts.exercises.exerciseId', select: ['name']});
-    console.log("update profile returning", updatedProfile);
-    return updatedProfile
-
+  let updatedProfile = await Profile.findOneAndUpdate(
+    { userId: uid },
+    updatedProfileDetails,
+    { returnOriginal: false }
+  ).populate({ path: "workouts.exercises.exerciseId", select: ["name"] });
+  console.log("update profile returning", updatedProfile);
+  return updatedProfile;
 }
